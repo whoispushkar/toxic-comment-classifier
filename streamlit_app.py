a00122f79dd8fbb99e64a8caa52d4fac2c3c9e12
Pushkar Sharma <pushkar0692@gmail.com>
2026-08-15 17:34:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # FastAPI Backend
 # --------------------------------------------------
 
-API_URL = "https://toxic-comment-classifier-7lee.onrender.com"
+API_URL = "https://toxic-comment-classifier-1-8dtu.onrender.com"
 
 # --------------------------------------------------
 # Custom CSS
